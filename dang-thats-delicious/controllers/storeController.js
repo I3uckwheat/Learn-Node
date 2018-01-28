406dc593a36460f9d7147a3c7a27166f38f3ea37
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-27 21:30:43 -0600

    removes middleware example :

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,9 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = "wes";
-  res.cookie('name', "Briggs is cool", {maxAge: 900000000000000})
-  next();
-}
-
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController')
 
 // Do work here
-router.get('/', storeController.myMiddleware, storeController.homePage)
+router.get('/', storeController.homePage)
 
 
 
