4fef34c918b883f1bf35a77038b502e88bf938f4
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-28 14:29:21 -0600

    adds viewing of stores

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose")
 const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 }
 
@@ -11,7 +10,12 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body)
-  await store.save();
-  res.redirect("/");
+  const store = await (new Store(req.body)).save()
+  req.flash("success", `Successfully created ${store.name}, care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', {title: 'Stores', stores})
 }
