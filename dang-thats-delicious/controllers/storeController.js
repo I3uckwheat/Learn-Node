6eff3d3caa0359f7a7b15a05f2572eba613ed164
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-27 21:51:48 -0600

    mixins

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -2,3 +2,9 @@ exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
 }
+
+exports.addStore = (req, res) => {
+  res.render('editStore', {
+    title: "Add Store"
+  })
+}
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -4,6 +4,7 @@ const storeController = require('../controllers/storeController')
 
 // Do work here
 router.get('/', storeController.homePage)
+router.get("/add", storeController.addStore)
 
 
 
