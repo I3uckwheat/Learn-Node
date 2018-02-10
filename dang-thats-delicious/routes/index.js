29fbddafe10b7ed93ad7753621c722739a68e6e4
Briggs Elsperger <i3uckwheat@gmail.com>
2018-02-10 09:04:38 -0600

    sets up passport

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const {catchErrors} = require("../handlers/errorHandlers");
 
@@ -32,7 +33,8 @@ router.get('/register', userController.registerForm);
 
 router.post('/register',
   userController.validateRegister,
-  userController.register
+  userController.register,
+  authController.login
 )
 
 
