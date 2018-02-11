d44653f6acc9a91634c30645dc23d502ea7aff1a
Briggs <briggs@localhost.localdomain>
2018-02-11 14:48:33 -0600

    sets up user password reset

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -51,4 +51,6 @@ router.get('/account',
 
 router.post('/account', catchErrors(userController.updateAccount)) //Insecure as user is not checked to be logged in?
 
+router.post('/account/forgot', catchErrors(authController.forgot));
+
 module.exports = router;
